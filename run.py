a450d0f1495dcb0ff8535dd7bb2d564419ef5ca9
ScriptLineStudios <scriptlinestudios@protonmail.com>
2024-03-16 09:11:15 +0200

    added config handler

--- run.py
+++ run.py
@@ -1,10 +1,25 @@
 import logging
-
-from src.managers import Manager
+import os
+import json
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S"
+    level=logging.DEBUG, format="%(asctime)s-%(levelname)s: %(message)s", datefmt="%d-%b-%y %H:%M:%S"
 )
 
+def handle_config():
+    with open("config.json", "w") as f:
+        f.write(json.dumps({
+            "discord_token": "",
+            "abuseipdb_token": "",
+            "mongo_uri": "",
+            "database_name": ""
+        }))
+    logging.error("No config file found! Please fill in the one we just created.")            
+    exit(1)
+
 if __name__ == "__main__":
+    if not os.path.exists("config.json"):
+        handle_config()
+
+    from src.managers import Manager
     Manager().start()
